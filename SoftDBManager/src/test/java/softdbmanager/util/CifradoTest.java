a8d4f8a55533e043449730a9664cb10b3f7133be
Joker <sfarfan777@gmail.com>
2025-06-13 00:24:11 -0500

    DB manager

---
+++ SoftDBManager/src/test/java/softdbmanager/util/CifradoTest.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package softdbmanager.util;
+
+import pe.edu.pucp.softdbmanager.util.Cifrado;
+import java.util.Properties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author salva
+ */
+public class CifradoTest {
+    //Properties properties = new Properties();
+    public CifradoTest() {
+    }
+    
+    Cifrado cifrado = new Cifrado();
+    
+    @Test
+    public void testCifrarMD5() {
+        String clave = "pass1";
+        String claveCifrada;
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        String claveDecifrada;
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+        clave = "pass2";
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+        clave = "pass3";
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+        clave = "pass4";
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+        clave = "pass5";
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+        
+        
+    }
+
+
+    
+}
