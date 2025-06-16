ce0aa2ccc0396caa6a16a0b216b0f57c3811c51a
Joker <sfarfan777@gmail.com>
2025-06-16 13:51:14 -0500

    Create UsuarioBO.java

---
+++ SoftCitBO/src/main/java/pe/edu/pucp/softcitbo/BO/UsuarioBO.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softcitbo.BO;
+
+import pe.edu.pucp.softcit.dao.UsuarioDAO;
+import pe.edu.pucp.softcit.daoImp.UsuarioDAOImpl;
+import pe.edu.pucp.softcit.model.EstadoGeneral;
+import pe.edu.pucp.softcit.model.EstadoLogico;
+import pe.edu.pucp.softcit.model.UsuarioDTO;
+
+/**
+ *
+ * @author salva
+ */
+public class UsuarioBO {
+    
+    private UsuarioDAO usuarioDao;
+    
+    
+    public UsuarioBO(){
+        usuarioDao = new UsuarioDAOImpl();
+    } 
+    
+    
+    public UsuarioDTO obtenerPorId(Integer id){
+        return this.usuarioDao.obtenerPorId(id);
+    }
+    public UsuarioDTO buscarCuenta(String nroDocumento,String tipoDoc,String contrasenha){
+        return this.usuarioDao.buscarCuenta(nroDocumento, tipoDoc, contrasenha);
+    }
+    public Integer insertar(UsuarioDTO usuario){
+        return this.usuarioDao.insertar(usuario);
+    }
+    public Integer modificar(UsuarioDTO usuario){
+        return this.usuarioDao.modificar(usuario);
+    }
+    public Integer cambiarEstadoGeneral(UsuarioDTO usuario,Integer estadoGeneral){
+        usuario.setEstadoGeneral(EstadoGeneral.valueOfCodigo(estadoGeneral));
+        return this.usuarioDao.cambiarEstadoGeneral(usuario);
+    }
+    
+    
+    public Integer cambiarEstadoLogico(UsuarioDTO usuario,Integer estadoLogico){
+        usuario.setEstadoLogico(EstadoLogico.valueOfCodigo(estadoLogico));
+        return this.usuarioDao.cambiarEstadoLogico(usuario);
+    }
+    
+    
+    public UsuarioDTO completarRoles(UsuarioDTO usuario){
+        return this.completarRoles(usuario);
+    }
+    
+    
+}
