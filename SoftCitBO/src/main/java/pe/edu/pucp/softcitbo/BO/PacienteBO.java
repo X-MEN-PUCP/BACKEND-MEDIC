b06fd97747bf88109b4a8b48f34310a090978a66
LuisAguilar0408 <a20182863@pucp.edu.pe>
2025-06-27 18:08:13 -0500

    cancelarCitaFunca

--- SoftCitBO/src/main/java/pe/edu/pucp/softcitbo/BO/PacienteBO.java
+++ SoftCitBO/src/main/java/pe/edu/pucp/softcitbo/BO/PacienteBO.java
@@ -79,6 +79,8 @@ public class PacienteBO {
         //actualizar cita (Estado: disponible)
         CitaDTO cita = historia_por_cita.getCita();
         cita.setEstado(EstadoCita.DISPONIBLE);
+        cita.setUsuarioModificacion(cita.getUsuarioModificacion());
+        cita.setFechaModificacion(LocalDate.now().toString());
         this.citaBO.modificar(cita);
         return this.historiaClinicaPorCitaBO.eliminar(historia_por_cita);
     }
