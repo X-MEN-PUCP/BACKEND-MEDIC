8a4620c48004c5ba25d69366e91116c659632277
Joker <sfarfan777@gmail.com>
2025-06-28 12:52:34 -0500

    Fix parameter order in listarPorEspecialidad call
    Corrected the order of parameters when calling listarUsuariosPorEspecialidadConFiltro in EspecialidadXUsuarioDAOImpl. This ensures the correct values are passed for user and specialty IDs.

--- SoftCitPersistance/src/main/java/pe/edu/pucp/softcit/daoImp/EspecialidadXUsuarioDAOImpl.java
+++ SoftCitPersistance/src/main/java/pe/edu/pucp/softcit/daoImp/EspecialidadXUsuarioDAOImpl.java
@@ -98,7 +98,7 @@ public class EspecialidadXUsuarioDAOImpl extends DAOImplBase implements Especial
     @Override
     public ArrayList<UsuarioPorEspecialidadDTO> listarPorEspecialidad(Integer idEspecialidad) {
         Integer idUsuario = null;
-        return listarUsuariosPorEspecialidadConFiltro(idEspecialidad, idUsuario);
+        return listarUsuariosPorEspecialidadConFiltro(idUsuario,idEspecialidad );
     }
 
     public ArrayList<UsuarioPorEspecialidadDTO> listarUsuariosPorEspecialidadConFiltro(Integer idUsuario, Integer idEspecialidad) {
